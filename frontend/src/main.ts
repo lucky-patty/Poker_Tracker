b671501065e822ce41c37bd495fb19a70a8656b5
lucky-patty <luck.wtj@gmail.com>
2019-08-28 06:38:54 -0700

    add vuetify in project structure

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,10 +2,17 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import './registerServiceWorker';
+import vuetify from './plugins/vuetify';
+
+// Import vuetify
+import Vuetify from 'vuetify/lib';
+
+Vue.use(Vuetify);
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app');
