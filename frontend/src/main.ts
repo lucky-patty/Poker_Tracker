d2c401743b254ecfce39f07c4eae5c17f879d721
lucky-patty <luck.wtj@gmail.com>
2019-08-29 04:36:29 -0700

    add poker index page with eslint standard

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,10 +3,13 @@ import App from './App.vue';
 import router from './router';
 import './registerServiceWorker';
 import vuetify from './plugins/vuetify';
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
 
 Vue.config.productionTip = false;
 
-Vue.use(vuetify);
+Vue.use(vuetify,{
+  iconfont: 'mdi',
+});
 
 new Vue({
   router,
--- frontend/src/views/PokerIndex.vue
+++ frontend/src/views/PokerIndex.vue
@@ -1,11 +1,45 @@
 <template>
-<v-container grid-list-md text-xs-center>
+<v-card flat>
+  <v-navigation-drawer clipped fixed v-model="drawer" dark>
+    <v-list dense>
+      <v-list-tile>
+        <v-list-tile-action>
+          <v-icon>dashboard</v-icon>
+        </v-list-tile-action>
+        <v-list-tile-content>
+          <v-list-tile-title>scoreboard</v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+  </v-navigation-drawer>
   <v-layout row wrap>
-    <v-flex xs12>
-      <v-card elevation='0'>
-        <h1>Welcome To Poker Score Keeper</h1>
-      </v-card>
-    </v-flex>
+    <v-toolbar fixed clipped-left dark>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer">
+      </v-toolbar-side-icon>
+      <v-toolbar-title>Poker Score Tracker</v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
   </v-layout>
-</v-container>
+  <!-- </v-container> -->
+</v-card>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: true,
+      items: [{
+        title: 'Scoreboard',
+        icon: 'dashboard',
+      },
+      {
+        title: 'Setting',
+        icon: 'setting',
+      },
+      ],
+      right: null,
+    };
+  },
+};
+</script>
