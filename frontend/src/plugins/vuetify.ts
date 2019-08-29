9e98ae44e3442540773d0fcbd264c3be965266e8
lucky-patty <luck.wtj@gmail.com>
2019-08-29 06:42:08 -0700

    add poker index page.vue

--- frontend/src/plugins/vuetify.ts
+++ frontend/src/plugins/vuetify.ts
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 // import Vuetify from 'vuetify/lib';
 import Vuetify from 'vuetify';
-import 'vuetify/dist/vuetify.min.css'
+import 'vuetify/dist/vuetify.min.css';
 import 'material-design-icons-iconfont/dist/material-design-icons.css'; // Ensure you are using css-loader
 import '@mdi/font/css/materialdesignicons.css'; // Ensure you are using css-loader
 
--- frontend/src/views/PokerIndex.vue
+++ frontend/src/views/PokerIndex.vue
@@ -1,8 +1,20 @@
 <template>
 <div>
-  <v-toolbar clipped-left dark>
-    <v-app-bar-nav-icon></v-app-bar-nav-icon>
-    <v-toolbar-title class="white--text">Title</v-toolbar-title>
+  <v-navigation-drawer clipped v-model="drawer" app dark>
+    <v-list dense nav>
+      <v-list-item v-for="item in items" :key="item.title" link>
+        <v-list-item-icon>
+          <v-icon>{{item.icon}}</v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title>{{item.title}}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+  <v-app-bar app fixed clipped-left dark>
+    <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+    <v-toolbar-title class="white--text">Poker Score Tracker</v-toolbar-title>
     <v-spacer></v-spacer>
     <v-toolbar-items>
       <v-btn icon flat>
@@ -12,7 +24,27 @@
         <v-icon>refresh</v-icon>
       </v-btn>
     </v-toolbar-items>
-  </v-toolbar>
+  </v-app-bar>
 
 </div>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: false,
+    items: [{
+      title: 'Scoreboard',
+      icon: 'dashboard',
+    },
+    {
+      title: 'Setting',
+      icon: 'settings',
+    },
+    ],
+  }),
+  props: {
+    source: String,
+  },
+};
+</script>
